cfbe9b4b33ffc7ea4900faefc140359a84da943b
dhenriquez <contacto@dhenriquez.cl>
2022-08-01 00:28:20 -0400

    funcion de tempertatura

--- custom.py
+++ custom.py
@@ -13,3 +13,13 @@ class Led(machine.Pin):  # type: ignore
     
     def toggle(self):
         self.led.toggle()
+
+class Temperature(machine.ADC):
+    
+    def __init__(self, port):
+        self.sensor = machine.ADC(port)
+        self.factor = 3.3 / (65535)
+    
+    def get(self):
+        reading = self.sensor.read_u16() * self.factor
+        return (27 - (reading - 0.706)/0.001721)
